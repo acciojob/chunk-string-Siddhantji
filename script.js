95b8c1910d92f9f9f316f1d9ef3a0086f6cb67fe
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-07 18:09:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function stringChop(str, size) {
     return chunks;
 }
 
-Do not change the code below
+// Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
